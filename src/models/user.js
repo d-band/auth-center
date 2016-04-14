6ef9c3e3f85a1d873da2f0262f3b3c35e3499034
振震 <865702942@qq.com>
2016-04-14 18:04:45 +0800

    login page changed

--- src/models/user.js
+++ src/models/user.js
@@ -41,8 +41,7 @@ export default function(sequelize, DataTypes) {
       findByName: function * (username) {
         return yield this.find({
           where: {
-            username: username,
-            enabled: 1
+            username: username
           }
         });
       },
