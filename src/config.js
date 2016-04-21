bf480e26ed64f6992828467b14ed1af407b9545d
振震 <865702942@qq.com>
2016-04-21 14:04:59 +0800

    totp template added

--- src/config.js
+++ src/config.js
@@ -30,10 +30,20 @@ const _config = {
     templates: {
       password_reset: {
         subject: 'Please reset your password',
-        html: '<p>Hello, <strong>{{username}}</strong>, we heard that you lost your password. Sorry about that!</p>' +
-          '<p>But don’t worry! You can use the following link to reset your password:</p>' +
+        html: '<p>Hello, <strong>{{username}}</strong>, we heard that you lost your password. Sorry about that!<br>' +
+          'But don’t worry! You can use the following link to reset your password:</p>' +
           '<p><a href="{{link}}">{{link}}</a></p>' +
           '<p>If you don’t use this link within {{ttl}} hours, it will expire.</p>' +
+          '<p>Thanks!</p>'
+      },
+      send_totp: {
+        subject: '[Important] The key of the dynamic password',
+        html: '<p>Hello, <strong>{{username}}</strong>, following image is the key for dynamic password.</p>' +
+          '<p><img src="cid:{{cid}}"/></p>' +
+          '<p>You should download <strong>Google Authenticator</strong> to use it.</p>' +
+          '<p>iOS App: <a href="https://itunes.apple.com/cn/app/google-authenticator/id388497605?mt=8">https://itunes.apple.com/cn/app/google-authenticator/id388497605?mt=8</a><br>' +
+          'Android App: <a href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=en">https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=en</a><br>' +
+          'Install Help: <a href="https://support.google.com/accounts/answer/1066447?hl=en">https://support.google.com/accounts/answer/1066447?hl=en</a></p>' +
           '<p>Thanks!</p>'
       }
     }
